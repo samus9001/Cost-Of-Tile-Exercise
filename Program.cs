80656fa6bbc89934434cb477aabf9c67f8d8dd1c
Samus9001 <sameer99@outlook.com>
2022-11-23 19:45:26 +0000

    Updated calculation code to correctly calculate cost when a decimal is used

--- Program.cs
+++ Program.cs
@@ -4,19 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, please enter size of the area in square feet!");
-            int area = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter width of 1 unit of flooring");
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter length of 1 unit of flooring");
-            int length = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter cost of 1 unit of flooring");
-            int cost = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the width of the area");
+            decimal width = Convert.ToDecimal(Console.ReadLine());
 
-            int areaSF = (length* width);
-            int unit = (area / areaSF);
-            int totalCost = (unit * cost);
+            Console.WriteLine("Enter the length of the area");
+            decimal length = Convert.ToDecimal(Console.ReadLine());
 
+            Console.WriteLine("Enter the cost of per square foot");
+            decimal cost = Convert.ToDecimal(Console.ReadLine());
+
+            decimal areaSF = (length * width);
+            decimal totalCost = (areaSF * cost);
             Console.WriteLine("The total cost to cover the area is £" + totalCost);
         }
     }
