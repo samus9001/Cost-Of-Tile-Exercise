bd0d04cadf7fb0c3ed3c591081c0a79f883bf468
Samus9001 <sameer99@outlook.com>
2022-12-12 19:15:26 +0000

    Replaced hard coded PI value with Math.PI Moved the cost of flooring prompt and the total cost output outside of the if statements to reduce duplicated code

--- Program.cs
+++ Program.cs
@@ -1,97 +1,89 @@
-﻿namespace CostOfTile
+﻿using System;
+
+namespace CostOfTile
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //constant decimal value for the average cost of labour rate per 20 square foot
             const decimal labourSF = 20;
-            //constant decimal value for the average cost of labour rate per hour
             const decimal labourPH = 86;
-            //value of pi
-            double pi = 3.14159;
-
-
+            decimal totalCostRounded = 0;
+     
             Console.WriteLine("What is the shape of the room? Please input one of the following: rectangle, circle, triangle");
             string UserInput = Console.ReadLine();
 
+            Console.WriteLine("Enter the cost of flooring per square foot");
+            decimal cost = Convert.ToDecimal(Console.ReadLine());
+
             if (UserInput == "rectangle")
             {
-                //user is prompted to enter a decimal value for width, length, and flooring cost per square foot
+                //user is prompted to enter a value for width and length
                 Console.WriteLine("Enter the width of the room flooring");
-                decimal width = Convert.ToDecimal(Console.ReadLine());
-
+                double width = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Enter the length of the room flooring");
-                decimal length = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Enter the cost of flooring per square foot");
-                decimal cost = Convert.ToDecimal(Console.ReadLine());
+                double length = Convert.ToDouble(Console.ReadLine());
 
                 //formula to calculate the square foot area of a rectangle
-                decimal rectangleAreaSF = (length * width);
+                double rectangleAreaSF = (length * width);
+                decimal rectangleAreaSFDecimal = Convert.ToDecimal(rectangleAreaSF);
 
                 //formula to calculate the cost of flooring
-                decimal flooringCost = (rectangleAreaSF * cost);
+                decimal flooringCost = (rectangleAreaSFDecimal * cost);
 
                 //formula to calculate the total labour cost
-                decimal labourCost = (rectangleAreaSF / labourSF) * labourPH;
-
-                //outputs the total cost to the user
-                Console.WriteLine($"The total cost to cover the flooring for the room is £{flooringCost + labourCost}");
+                decimal labourCost = (rectangleAreaSFDecimal / labourSF) * labourPH;
+                decimal totalCost = flooringCost + labourCost;
+                totalCostRounded = Math.Round(totalCost, 2);
             }
 
             if (UserInput == "circle")
             {
-                //user is prompted to enter a decimal value for the radius of the circle and flooring cost per square foot
+                //user is prompted to enter a value for the radius of the circle
                 Console.WriteLine("Enter the radius of the room flooring");
-                decimal radius = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Enter the cost of flooring per square foot");
-                decimal cost = Convert.ToDecimal(Console.ReadLine());
+                double radius = Convert.ToDouble(Console.ReadLine());
 
                 //calculate the square of the radius
-                decimal radiusSquared = radius * radius;
-
-                //convert the double value of pi to a decimal value
-                decimal piDecimal = Convert.ToDecimal(pi);
+                double radiusSquared = radius * radius;
 
                 //formula to calculate the square foot area of a circle
-                decimal circleAreaSF = (piDecimal * radiusSquared);
+                double circleAreaSF = (Math.PI * radiusSquared);
+                decimal circleAreaSFDecimal = Convert.ToDecimal(circleAreaSF);
 
                 //formula to calculate the cost of flooring
-                decimal flooringCost = (circleAreaSF * cost);
+                decimal flooringCost = (circleAreaSFDecimal * cost);
 
                 //formula to calculate the total labourCost
-                decimal labourCost = (circleAreaSF / labourSF) * labourPH;
+                decimal labourCost = (circleAreaSFDecimal / labourSF) * labourPH;
+                decimal totalCost = flooringCost + labourCost;
+                totalCostRounded = Math.Round(totalCost, 2);
 
-                //outputs the total cost to the user
-                Console.WriteLine($"The total cost to cover the flooring for the room is £{flooringCost + labourCost}");
             }
 
             if (UserInput == "triangle")
             {
-                //user is prompted to enter a decimal value for base length, height, and flooring cost per square foot
+                //user is prompted to enter a value for base length and height
                 Console.WriteLine("Enter the base length of the room flooring");
-                decimal length = Convert.ToDecimal(Console.ReadLine());
-
+                double length = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Enter the height of the room flooring");
-                decimal height = Convert.ToDecimal(Console.ReadLine());
-
-                Console.WriteLine("Enter the cost of flooring per square foot");
-                decimal cost = Convert.ToDecimal(Console.ReadLine());
+                double height = Convert.ToDouble(Console.ReadLine());
 
                 //formula to calculate the square foot area of a triangle
-                decimal triangleAreaSF = ((1.0m / 2.0m) * length * height);
+                double triangleAreaSF = ((1 / 2) * length * height);
+                decimal triangleAreaSFDecimal = Convert.ToDecimal(triangleAreaSF);
 
                 //formula to calculate the cost of flooring
-                decimal flooringCost = (triangleAreaSF * cost);
+                decimal flooringCost = (triangleAreaSFDecimal * cost);
 
                 //formula to calculate the total labourCost
-                decimal labourCost = (triangleAreaSF / labourSF) * labourPH;
+                decimal labourCost = (triangleAreaSFDecimal / labourSF) * labourPH;
+                decimal totalCost = flooringCost + labourCost;
+                totalCostRounded = Math.Round(totalCost, 2);
 
-                //outputs the total cost to the user
-                Console.WriteLine($"The total cost to cover the flooring for the room is £{flooringCost + labourCost}");
             }
+
+            //outputs the total cost to the user
+            Console.WriteLine($"The total cost to cover the flooring for the room is £{totalCostRounded}");
         }
     }
 }
