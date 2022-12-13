d4db17bbc6df151dd28d5a8d8f48b8a225cf9271
Samus9001 <sameer99@outlook.com>
2022-12-13 00:30:01 +0000

    Added a new line space and used rounding to 2 d.p for total costs for readability

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace CostOfTile
 {
@@ -14,63 +15,72 @@ namespace CostOfTile
             decimal totalCost = 0;
             decimal totalCostRounded = 0;
 
-            Console.WriteLine("What is the shape of the room? Please input one of the following: rectangle, circle, triangle");
-            string UserInput = Console.ReadLine();
-
-            Console.WriteLine("Enter the cost of flooring per square foot");
-            decimal cost = Convert.ToDecimal(Console.ReadLine());
-
-            if (UserInput == "rectangle")
+            while (true)
             {
-                //user is prompted to enter a value for width and length
-                Console.WriteLine("Enter the width of the room flooring");
-                double width = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter the length of the room flooring");
-                double length = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("What is the shape of the room? Please input one of the following: rectangle, circle, triangle");
+                string UserInput = Console.ReadLine();
 
-                //formula to calculate the square foot area of a rectangle
-                double area = (length * width);
-                areaDecimal = Convert.ToDecimal(area);
+                if (UserInput != "rectangle" && UserInput != "circle" && UserInput != "triangle")
+                {
+                    Console.WriteLine("\nThat is not a valid option\n");
+                    continue;
+                }
+
+                Console.WriteLine("\nEnter the cost of flooring per square foot");
+                decimal cost = Convert.ToDecimal(Console.ReadLine());
+
+                if (UserInput == "rectangle")
+                {
+                    //user is prompted to enter a value for width and length
+                    Console.WriteLine("\nEnter the width of the room flooring");
+                    double width = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nEnter the length of the room flooring");
+                    double length = Convert.ToDouble(Console.ReadLine());
+
+                    //formula to calculate the square foot area of a rectangle
+                    double area = (length * width);
+                    areaDecimal = Convert.ToDecimal(area);
+                }
+
+                if (UserInput == "circle")
+                {
+                    //user is prompted to enter a value for the radius of the circle
+                    Console.WriteLine("\nEnter the radius of the room flooring");
+                    double radius = Convert.ToDouble(Console.ReadLine());
+
+                    //calculate the radius squared
+                    double radiusSquared = radius * radius;
+
+                    //formula to calculate the square foot area of a circle
+                    double area = (Math.PI * radiusSquared);
+                    areaDecimal = Convert.ToDecimal(area);
+                }
+
+                if (UserInput == "triangle")
+                {
+                    //user is prompted to enter a value for base length and height
+                    Console.WriteLine("\nEnter the base length of the room flooring");
+                    double length = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\nEnter the height of the room flooring");
+                    double height = Convert.ToDouble(Console.ReadLine());
+
+                    //formula to calculate the square foot area of a triangle
+                    double area = ((1.0 / 2.0) * length * height);
+                    areaDecimal = Convert.ToDecimal(area);
+                }
+
+                //formula to calculate the cost of flooring
+                flooringCost = (areaDecimal * cost);
+
+                //formula to calculate the total labourCost
+                labourCost = (areaDecimal / labourSF) * labourPH;
+                totalCost = flooringCost + labourCost;
+
+                //outputs the total cost to the user
+                totalCostRounded = Math.Round(totalCost, 2);
+                Console.WriteLine($"\nThe total cost to cover the flooring for the room is £{totalCostRounded}");
+                break;
             }
-
-            if (UserInput == "circle")
-            {
-                //user is prompted to enter a value for the radius of the circle
-                Console.WriteLine("Enter the radius of the room flooring");
-                double radius = Convert.ToDouble(Console.ReadLine());
-
-                //calculate the radius squared
-                double radiusSquared = radius * radius;
-
-                //formula to calculate the square foot area of a circle
-                double area = (Math.PI * radiusSquared);
-                areaDecimal = Convert.ToDecimal(area);
-            }
-
-            if (UserInput == "triangle")
-            {
-                //user is prompted to enter a value for base length and height
-                Console.WriteLine("Enter the base length of the room flooring");
-                double length = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Enter the height of the room flooring");
-                double height = Convert.ToDouble(Console.ReadLine());
-
-                //formula to calculate the square foot area of a triangle
-                double area = ((1.0 / 2.0) * length * height);
-                Console.WriteLine(area);
-                areaDecimal = Convert.ToDecimal(area);
-            }
-
-            //formula to calculate the cost of flooring
-            flooringCost = (areaDecimal * cost);
-
-            //formula to calculate the total labourCost
-            labourCost = (areaDecimal / labourSF) * labourPH;
-            totalCost = flooringCost + labourCost;
-
-            //outputs the total cost to the user
-            totalCostRounded = Math.Round(totalCost, 2);
-            Console.WriteLine($"The total cost to cover the flooring for the room is £{totalCostRounded}");
         }
     }
 }
